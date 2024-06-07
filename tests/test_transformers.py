aecb0c948889de97cf55a0c34bf04532385a05bf
Ruben Lopez <rjlopez2@gmail.com>
2024-06-08 00:23:19 +0200

    reformat with ruff

--- tests/test_transformers.py
+++ tests/test_transformers.py
@@ -10,15 +10,17 @@ url = "https://perritos.myasustor.com:1985/data/img_id_1550.jpg"
 response = requests.get(url)  # noqa: S113
 img = Image.open(BytesIO(response.content))
 
-def test_from_LAB_to_RGB_img(img_test:Any = img)-> None:
+
+def test_from_LAB_to_RGB_img(img_test: Any = img) -> None:
     # retrieve a sample image
     img_test_np = np.asarray(img_test)
     x_s, y_s, _ = img_test_np.shape
-    r, g, b  = (img_test_np[:,:,i].reshape((x_s*y_s, 1)) for i in range(3))
-    l, a, b = to.RGB2LAB(r, g.reshape(-1, 1), b.reshape(-1, 1) )  # noqa: E741
-    ab = np.concatenate((a, b), axis = 1).reshape((x_s, y_s, -1))
+    r, g, b = (img_test_np[:, :, i].reshape((x_s * y_s, 1)) for i in range(3))
+    l, a, b = to.RGB2LAB(r, g.reshape(-1, 1), b.reshape(-1, 1))  # noqa: E741
+    ab = np.concatenate((a, b), axis=1).reshape((x_s, y_s, -1))
     new_img = to.from_LAB_to_RGB_img(l, ab)
     assert np.allclose(img_test_np, new_img.reshape((x_s, y_s, -1)))
+
 
 def test_RGB2LAB() -> None:
     # this is just a dumb test.
@@ -30,6 +32,7 @@ def test_RGB2LAB() -> None:
     # r, g, b  = (img_test_np[:,:,i].reshape((x_s*y_s, 1)) for i in range(3))
     # _ = to.RGB2LAB(r, g.reshape(-1, 1), b.reshape(-1, 1) )
     assert True
+
 
 def test_LAB22RGB() -> None:
     # this is just a dumb test.
@@ -51,35 +54,33 @@ def test_plot_multiple_imgs_test() -> None:
     assert True
 
 
-def test_psnr_test(img_test:Any = img) -> None:
-     # this is just a dumb test.
-     img_test_np = np.asarray(img_test)
-     rms_img_test = to.psnr(imageA=img_test_np, imageB=img_test_np)
+def test_psnr_test(img_test: Any = img) -> None:
+    # this is just a dumb test.
+    img_test_np = np.asarray(img_test)
+    rms_img_test = to.psnr(imageA=img_test_np, imageB=img_test_np)
 
-     assert np.allclose(rms_img_test, rms_img_test)
-
-
-def test_mse_test(img_test:Any = img) -> None:
-     # this is just a dumb test.
-     img_test_np = np.asarray(img_test)
-     rms_img_test = to.mse(imageA=img_test_np, imageB=img_test_np, nband=2)
-
-     assert np.allclose(rms_img_test, rms_img_test)
+    assert np.allclose(rms_img_test, rms_img_test)
 
 
-def test_mae_test(img_test:Any = img) -> None:
-     # this is just a dumb test.
-     img_test_np = np.asarray(img_test)
-     rms_img_test = to.mae(imageA=img_test_np, imageB=img_test_np, nband=2)
+def test_mse_test(img_test: Any = img) -> None:
+    # this is just a dumb test.
+    img_test_np = np.asarray(img_test)
+    rms_img_test = to.mse(imageA=img_test_np, imageB=img_test_np, nband=2)
 
-     assert np.allclose(rms_img_test, rms_img_test)
-
-
-def test_rmse_test(img_test:Any = img) -> None:
-     # this is just a dumb test.
-     img_test_np = np.asarray(img_test)
-     rms_img_test = to.rmse(imageA=img_test_np, imageB=img_test_np, nband=2)
-
-     assert np.allclose(rms_img_test, rms_img_test)
+    assert np.allclose(rms_img_test, rms_img_test)
 
 
+def test_mae_test(img_test: Any = img) -> None:
+    # this is just a dumb test.
+    img_test_np = np.asarray(img_test)
+    rms_img_test = to.mae(imageA=img_test_np, imageB=img_test_np, nband=2)
+
+    assert np.allclose(rms_img_test, rms_img_test)
+
+
+def test_rmse_test(img_test: Any = img) -> None:
+    # this is just a dumb test.
+    img_test_np = np.asarray(img_test)
+    rms_img_test = to.rmse(imageA=img_test_np, imageB=img_test_np, nband=2)
+
+    assert np.allclose(rms_img_test, rms_img_test)
